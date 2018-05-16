12f21dd9f881a1f00fb13bd3db249c05cf9a202e
dangwei_90 <38289144+dangwei-90@users.noreply.github.com>
2018-05-16 16:32:06 +0800

    Create 412.Fizz Buzz.cpp

---
+++ 412.Fizz Buzz.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        vector<string> str;
+        for(int i = 1; i <= n; i ++)
+        {
+            if((i % 3 == 0) && (i % 5 == 0))
+            {
+                str.emplace_back("FizzBuzz");
+            }
+            else if(i % 3 == 0)
+            {
+                str.emplace_back("Fizz");
+            }
+            else if(i % 5 == 0)
+            {
+                str.emplace_back("Buzz");
+            }
+            else
+            {
+                str.emplace_back(to_string(i));
+            }
+        }
+        
+        return str;
+    }
+};
