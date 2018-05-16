dad2aa8957e34d00b4a04e1f1c74b650a10d57e0
dangwei_90 <38289144+dangwei-90@users.noreply.github.com>
2018-05-16 16:30:57 +0800

    1.搜索二叉树的定义 2.递归

---
+++ 669.Trim a Binary Search Tree.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* trimBST(TreeNode* root, int L, int R) {
+        // 参考递归
+        if(!root)
+        {
+            return nullptr;
+        }
+        
+        if(root->val < L)
+        {
+            root = trimBST(root->right, L, R);
+            return root;
+        }
+        else if(root->val > R)
+        {
+            root = trimBST(root->left, L, R);
+            return root;
+        }
+        else
+        {
+            root->right = trimBST(root->right, L, R);
+            root->left = trimBST(root->left, L, R);
+            return root;
+        }
+    }
+};
