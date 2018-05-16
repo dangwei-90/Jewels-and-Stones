7dba07dd73c6a1f18f41a8716e627c051f81562f
dangwei_90 <38289144+dangwei-90@users.noreply.github.com>
2018-05-16 16:29:12 +0800

    Create 832.Flipping an Image.cpp

---
+++ 832.Flipping an Image.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<vector<int>> flipAndInvertImage(vector<vector<int>>& A) {
+        vector<vector<int>> finalImage;
+        finalImage = A;
+        if(1)
+        {
+            for(int n = 0; n < A.size(); n++)
+            {
+                for(int m = 0; m < A[n].size(); m++)
+                {
+                    finalImage[n][m] = A[n][(A[n].size() - m - 1)];
+                    if(1 == finalImage[n][m])
+                    {
+                        finalImage[n][m] = 0;
+                    }
+                    else
+                    {
+                        finalImage[n][m] = 1;
+                    }
+                }
+            }
+        }
+        else
+        {
+            
+        }
+        
+        return finalImage;
+    }
+};
