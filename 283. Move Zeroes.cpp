9b53bcb9cfa8362f4bae93f0538a911acfde4ef8
dangwei_90 <38289144+dangwei-90@users.noreply.github.com>
2018-05-18 18:28:51 +0800

    不拷贝新的vector实现数据Move

--- 283. Move Zeroes.cpp
+++ 283. Move Zeroes.cpp
@@ -1,6 +1,23 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
+        int nmove = 0;
+        for(int n = 0; n < nums.size(); n++)
+        {
+            if(nums[n] != 0)
+            {
+                nums[nmove] = nums[n];
+                nmove++;
+            }
+        }
+        
+        for(int m = 1; m <= (nums.size() - nmove); m++)
+        {
+            nums[nums.size() - m] = 0;
+        }
+        
+        // my first solution
+        /*
         vector<int> move;
         int nZero = 0;
         for(int n = 0; n < nums.size(); n++)
@@ -20,5 +37,6 @@ public:
         }
         
         nums = move;
+        */
     }
 };
