8bfbe096249c974a54245f4da01c2e7ed576ef8a
dangwei_90 <38289144+dangwei-90@users.noreply.github.com>
2018-05-21 19:57:19 +0800

    ***** 注意题意；注意set的使用，set 不允许重复值，且从小到大排序

--- 349. Intersection of Two Arrays.cpp
+++ 349. Intersection of Two Arrays.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        // set 不允许重复值，且从小到大排序
         set<int> se(nums1.begin(), nums1.end()), se2;
         for(auto au : nums2)
         {
