568a01716b2e17c209d9275561c75dde66e4ddb7
dangwei_90 <38289144+dangwei-90@users.noreply.github.com>
2018-06-19 08:33:40 +0800

    用unordered_set+for速度没有嵌套for快

--- 771.Jewels and Stones.cpp
+++ 771.Jewels and Stones.cpp
@@ -2,6 +2,19 @@ class Solution {
 public:
     int numJewelsInStones(string J, string S) {
         int nCount = 0;
+        unordered_set<char> setstr(J.begin(), J.end());
+        for(char ch : S)
+        {
+            if(setstr.count(ch) > 0)
+            {
+                nCount++;
+            }
+        }
+        return nCount;
+        
+        // my solution
+        /*
+        int nCount = 0;
         for(int n = 0; n < S.length(); n++ )
         {
             for(int j = 0; j< J.length(); j++)
@@ -13,5 +26,6 @@ public:
             }
         }
         return nCount;
+        */
     } 
 };
